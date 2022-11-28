6536f6e7acc417ea15c01829baca7a1a7dd12d20
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-11-28 12:44:19 -0800

    changes before save

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,18 +1,36 @@
-import React from 'react'
-
+import React, { useState } from 'react';
 // Suggested initial states
-const initialMessage = ''
-const initialEmail = ''
-const initialSteps = 0
-const initialIndex = 4 // the index the "B" is at
+const initialMessage = '';
+const initialEmail = '';
+const initialSteps = 0;
+const initialIndex = 4; // the index the "B" is at
 
+const initialGrid = [
+  false,
+  false,
+  false,
+  false,
+  true,
+  false,
+  false,
+  false,
+  false,
+];
+
+const nextIndexes = [
+  [1, 4], // (1,1) - down or right
+  [0, 3], // (1,2) - left or right
+  [2, 5], // (1,3) - left or down
+];
 export default function AppFunctional(props) {
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
+  const [grid, setGrid] = useState(initialGrid);
 
   function getXY() {
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
+    return grid.indexOf(true);
   }
 
   function getXYMessage() {
@@ -29,11 +47,13 @@ export default function AppFunctional(props) {
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
+    const currentXY = getX();
   }
 
   function move(evt) {
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
+    console.log(getXY());
   }
 
   function onChange(evt) {
@@ -43,36 +63,41 @@ export default function AppFunctional(props) {
   function onSubmit(evt) {
     // Use a POST request to send a payload to the server.
   }
-
   return (
-    <div id="wrapper" className={props.className}>
-      <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+    <div id='wrapper' className={props.className}>
+      <div className='info'>
+        <h3 id='coordinates'>Coordinates (2, 2)</h3>
+        <h3 id='steps'>You moved 0 times</h3>
       </div>
-      <div id="grid">
-        {
-          [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
-            </div>
-          ))
-        }
+      <div id='grid'>
+        {grid.map((idx, i) => (
+          <div key={i} className={`square${idx === true ? ' active' : ''}`}>
+            {idx === true ? 'B' : null}
+          </div>
+        ))}
       </div>
-      <div className="info">
-        <h3 id="message"></h3>
+      <div className='info'>
+        <h3 id='message'></h3>
       </div>
-      <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
+      <div id='keypad'>
+        <button id='left' onClick={move}>
+          LEFT
+        </button>
+        <button id='up' onClick={move}>
+          UP
+        </button>
+        <button id='right' onClick={move}>
+          RIGHT
+        </button>
+        <button id='down' onClick={move}>
+          DOWN
+        </button>
+        <button id='reset'>reset</button>
       </div>
       <form>
-        <input id="email" type="email" placeholder="type email"></input>
-        <input id="submit" type="submit"></input>
+        <input id='email' type='email' placeholder='type email'></input>
+        <input id='submit' type='submit'></input>
       </form>
     </div>
-  )
+  );
 }
